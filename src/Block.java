8af351b15d9bf9001407cb0622570d3962779227
Vladislav7589 <vlad_islav_7589@mail.ru>
2021-12-18 12:38:26 +0600

    inital commit

---
+++ src/Block.java
@@ -0,0 +1,260 @@
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+
+interface Copyable{
+    Object copy();
+}
+class Image implements Copyable{
+    BufferedImage imageCopy;
+    public  Image(BufferedImage imageCopy){
+        this.imageCopy = imageCopy;
+    }
+
+    @Override
+    public Object copy() {
+        Image copy = new Image(imageCopy);
+        return copy;
+    }
+}
+
+public class Block extends JFrame {
+
+    JSplitPane main, leftSplit;
+    JPanel rightInstrument, downImage, upImage;
+    JMenuBar menuBar;
+    JMenuItem menuExit, menuSave, menuOpen;
+    static JLabel sourceImage,copyImage;
+    static JScrollPane upPanel, downPanel;
+    String fileName;
+    BufferedImage imag = null, rezult = null, copied;
+
+    JComboBox comboBox = new JComboBox();
+
+
+
+    private Block() throws IOException {
+        super("Effects");
+        setSize(1500, 900);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createLayout();
+
+        menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        menuOpen = new JMenuItem("Открыть");
+        fileMenu.add(menuOpen);
+        menuSave = new JMenuItem("Cохранить");
+        fileMenu.add(menuSave);
+        menuExit = new JMenuItem("Выход");
+        fileMenu.add(menuExit);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+        setVisible(true);
+
+
+
+        menuExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        menuSave.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(rezult != null) {
+                    try {
+                        JFileChooser jf = new JFileChooser();
+                        // Создаем фильтры для файлов
+                        TextFileFilter pngFilter = new TextFileFilter(".png");
+                        TextFileFilter jpgFilter = new TextFileFilter(".jpg");
+                        // Добавляем фильтры
+                        jf.addChoosableFileFilter(pngFilter);
+                        jf.addChoosableFileFilter(jpgFilter);
+                        int result = jf.showSaveDialog(null);
+                        if (result == JFileChooser.APPROVE_OPTION) {
+                            fileName = jf.getSelectedFile().getAbsolutePath();
+                        }
+                        // Смотрим какой фильтр выбран
+                        if (jf.getFileFilter() == pngFilter) {
+                            ImageIO.write(rezult, "png", new File(fileName + ".png"));
+                            JOptionPane.showMessageDialog(null, "Картинка сохранена!");
+                        } else {
+                            ImageIO.write(rezult, "jpeg", new File(fileName + ".jpg"));
+                            JOptionPane.showMessageDialog(null, "Картинка сохранена!");
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                else JOptionPane.showMessageDialog(null, "Нечего сохранять!");
+            }
+        });
+        menuOpen.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jf = new  JFileChooser();
+                int  result = jf.showOpenDialog(null);
+                if(result==JFileChooser.APPROVE_OPTION) {
+                    fileName = jf.getSelectedFile().getAbsolutePath();
+                    File iF = new File(fileName);
+                    jf.addChoosableFileFilter(new TextFileFilter(".png"));
+                    jf.addChoosableFileFilter(new TextFileFilter(".jpg"));
+                    try {
+                        imag = ImageIO.read(iF);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    Image original = new Image(imag);
+
+                    // создание сплит
+                    sourceImage = new JLabel(new ImageIcon(imag));
+                    upPanel = new JScrollPane(sourceImage);
+                    upPanel.setSize(760, 500);
+                    // копирование картинки
+
+                    Image copy = (Image)original.copy();
+                    copied = copy.imageCopy;
+
+                    copyImage = new JLabel(new ImageIcon(copied));
+                    downPanel = new JScrollPane(copyImage);
+
+                    leftSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, upPanel, downPanel);
+                    leftSplit.setDividerLocation(400);
+                    getContentPane().removeAll();
+                    validate();
+                    main = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftSplit, rightInstrument);
+                    main.setDividerLocation(800);
+                    add(main);
+                    rezult=null;
+                    setVisible(true);
+                }
+            }
+        });
+
+    }
+
+
+    private void createLayout() throws IOException {
+        createPanels();
+
+        leftSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, upPanel, null);
+        leftSplit.setDividerLocation(500);
+        leftSplit.setContinuousLayout(true);
+        leftSplit.setOneTouchExpandable(true);
+        main = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftSplit, rightInstrument);
+        main.setDividerLocation(1075);
+        main.setContinuousLayout(true);
+        main.setOneTouchExpandable(true);
+        getContentPane().add(main);
+    }
+
+    private void createPanels() throws IOException {
+        createUP();
+        createDown();
+        createInstruments();
+
+    }
+    private void createUP() throws IOException {
+        upImage = new JPanel(new GridLayout(1, 1));
+        upImage.setMinimumSize(new Dimension(600, 100));
+        BufferedImage source = ImageIO.read(new File("maxresdefault.jpg"));
+        sourceImage = new JLabel(new ImageIcon(source));
+        upPanel = new JScrollPane(sourceImage);
+    }
+
+    private void createDown() {
+        downImage = new JPanel(new GridLayout(1, 1));
+        downImage.setMinimumSize(new Dimension(600, 100));
+    }
+
+    private void createInstruments() {
+        rightInstrument = new JPanel(new GridLayout(20, 7));
+        comboBox.setEditable(true);
+        comboBox.addItem("Чёрно-белое");
+        comboBox.addItem("Сепия");
+        comboBox.addItem("Штамп");
+        comboBox.addItem("Негатив");
+
+
+        JButton button= new JButton("Выполнить");
+        Font two = new Font("Serif", Font.BOLD, 18);
+        button.setFont(two);
+        comboBox.setFont(two);
+        rightInstrument.add(comboBox);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Object selectedItem = comboBox.getSelectedItem();
+
+                if (imag != null) {
+
+                    switch (Objects.requireNonNull(selectedItem).toString()){
+                        case "Чёрно-белое": {
+                            Effects ef = new Effects(copied, 1);
+                            rezult = ef.res;
+                        };break;
+                        case "Штамп": {
+                            Effects ef = new Effects(copied, 3);
+                            rezult = ef.res;
+                        };break;
+                        case "Негатив": {
+                            Effects ef = new Effects(copied, 2);
+                            rezult = ef.res;
+                        };break;
+                        case "Сепия": {
+                            Effects ef = new Effects(copied, 4);
+                            rezult = ef.res;
+                        };break;
+                    }
+                    Settings();
+                }
+                else JOptionPane.showMessageDialog(null, "Картинка не открыта");
+            }
+        });
+        rightInstrument.add(button);
+    }
+
+    private void Settings() {
+        JLabel right = new JLabel(new ImageIcon(rezult));
+        downPanel = new JScrollPane(right);
+        leftSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, upPanel, downPanel);
+        leftSplit.setDividerLocation(400);
+        getContentPane().removeAll();
+        validate();
+        main = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftSplit, rightInstrument);
+        main.setDividerLocation(800);
+        add(main);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+static class TextFileFilter extends FileFilter
+{
+    private String ext;
+    TextFileFilter(String ext)
+    {
+        this.ext=ext;
+    }
+    public boolean accept(java.io.File file)
+    {
+        if (file.isDirectory()) return true;
+        return (file.getName().endsWith(ext));
+    }
+    public String getDescription()
+    {
+        return "*"+ext;
+    }
+}
+
+    public static void main(String[] args) throws IOException {
+        new Block();
+    }
+}
---
+++ src/Effects.java
@@ -0,0 +1,142 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Effects {
+
+    public BufferedImage res;
+
+    public Effects(BufferedImage bm, int ef) {
+        if (ef == 1) {
+            this.res = blackWhite(bm);
+        } else if (ef == 2) {
+            this.res = negative(bm);
+        } else if (ef == 3) {
+            this.res = shtamp(bm);
+        } else if (ef == 4) {
+            this.res = sepia(bm);
+        }
+    }
+
+
+    public BufferedImage blackWhite(BufferedImage bm) {
+        BufferedImage result = new BufferedImage(bm.getWidth(), bm.getHeight(), bm.getType());
+
+        for (int x = 0; x < bm.getWidth(); x++) {
+            for (int y = 0; y < bm.getHeight(); y++) {
+
+                Color color = new Color(bm.getRGB(x, y));
+
+                int blue = color.getBlue();
+                int red = color.getRed();
+                int green = color.getGreen();
+
+                // Применяем стандартный алгоритм для получения черно-белого изображения
+                int grey = (int) (red + green + blue) / 3;
+
+                int newRed = grey;
+                int newGreen = grey;
+                int newBlue = grey;
+
+                // создаём новый цвет
+                Color newColor = new Color(newRed, newGreen, newBlue);
+
+                // И устанавливаем этот цвет в текущий пиксель результирующего изображения
+                result.setRGB(x, y, newColor.getRGB());
+            }
+        }
+        return result;
+    }
+
+    public BufferedImage negative(BufferedImage img) {
+        BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        int width = img.getWidth();
+        int height = img.getHeight();
+        // Преобразовать в отрицательный
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = img.getRGB(x, y);
+                int a = (p >> 24) & 0xff;
+                int r = (p >> 16) & 0xff;
+                int g = (p >> 8) & 0xff;
+                int b = p & 0xff;
+                // вычитаем RGB из 255
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+                // установить новое значение RGB
+                p = (a << 24) | (r << 16) | (g << 8) | b;
+                result.setRGB(x, y, p);
+            }
+        }
+        return result;
+    }
+
+    public BufferedImage shtamp(BufferedImage img) {
+        BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        for (int x = 0; x < img.getWidth(); x++)
+            for (int y = 0; y < img.getHeight(); y++)
+                if (rgbToGray(img.getRGB(x, y)) > 50)
+                    result.setRGB(x, y, 0);
+                else
+                    result.setRGB(x, y, 0xffffff);
+
+        return result;
+    }
+
+    private static int rgbToGray(int rgb) {
+        // split rgb integer into R, G and B components
+        int r = (rgb >> 16) & 0xff;
+        int g = (rgb >> 8) & 0xff;
+        int b = rgb & 0xff;
+        int gray;
+        gray = Math.round((Math.max(r, Math.max(g, b)) + Math.min(r, Math.min(g, b))) / 2);
+        return gray;
+    }
+
+
+    public BufferedImage sepia(BufferedImage img) {
+        BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = img.getRGB(x, y);
+                int a = (p >> 24) & 0xff;
+                int r = (p >> 16) & 0xff;
+                int g = (p >> 8) & 0xff;
+                int b = p & 0xff;
+
+                int tr = (int)(0.393*r + 0.769*g + 0.189*b);
+                int tg = (int)(0.349*r + 0.686*g + 0.168*b);
+                int tb = (int)(0.272*r + 0.534*g + 0.131*b);
+
+                //check condition
+                if(tr > 255){
+                    r = 255;
+                }else{
+                    r = tr;
+                }
+
+                if(tg > 255){
+                    g = 255;
+                }else{
+                    g = tg;
+                }
+
+                if(tb > 255){
+                    b = 255;
+                }else{
+                    b = tb;
+                }
+
+                //set new RGB value
+                p = (a<<24) | (r<<16) | (g<<8) | b;
+
+                result.setRGB(x, y, p);
+            }
+        }
+        return result;
+    }
+}
